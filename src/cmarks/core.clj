1f16d3a7a09152b566d47e1fdc9fda7b33383ed7
Adam Lehenbauer <adam.lehenbauer@gmail.com>
2011-06-22 16:10:11 -0400

    moves some functions around?

--- src/cmarks/core.clj
+++ src/cmarks/core.clj
@@ -1,76 +1,20 @@
 (ns cmarks.core
-  (:require [cmarks.data :as data])
-  (:require [net.cgrand.enlive-html :as html])
-  (:require [clojure.string :as string])
-  (:require [clojure.java.io :as jio])
-  (:require [clojure.contrib.logging :as logger]))
+  (:require 
+    [cmarks.data :as data]
+    [cmarks.titles :as titles]
+    [net.cgrand.enlive-html :as html]
+    [clojure.string :as string]
+    [clojure.java.io :as jio]
+    [clojure.contrib.logging :as logger]
+    [clojure.contrib.pprint :as pprint]))
 
 (defn print-records
   []
-  (let [records (data/select-from-db)]
+  (let [records (data/filtered-from-db)]
     (map #(println (str (:url %) " " (:title %))) records))) 
 
-(def *cache-dir*
-  ; create dir and return dir name
-  (do
-    (doto (java.io.File. "cache/") (.mkdir))
-    "cache/"))
-
-(defn strip-protocol
-  [url]
-  (cond 
-    (.startsWith url "https://") (string/replace url "https://" "")
-    (.startsWith url "http://") (string/replace url "http://" "")))
-
-(defn strip-slash
-  [url]
-  ;(if (.endsWith url "/") (.substring url 0 (dec (.length url)))))
-  (string/replace url "/" "."))
-
-(defn url-to-cachename
-  [url] ;string
-  (str *cache-dir*
-    (strip-slash (strip-protocol url))))
-
-(defn find-in-cache
-  [url] ;str
-  (let [f (jio/as-file (url-to-cachename url))]
-    (if (.exists f)
-      (html/html-resource f)
-      nil)))
-
-(defn cache-url
-  [url] ;string
-  (try
-    (jio/copy 
-      (jio/input-stream (jio/as-url url)) 
-      (jio/as-file (url-to-cachename url)))
-    (catch  java.net.MalformedURLException e
-      (println (str "MalformedURL for [" url "]")))
-    (catch java.io.IOException e
-      (println (.getMessage e)))))
-
-(defn cache-and-get
-  [url] ;str
-  (do
-    (cache-url url)
-    ;saves to disk, then reads back in
-    ;a little extra work, but no biggie
-    (find-in-cache url)))
-
-(defn find-resource
-  [url]
-  (let [from-cache (find-in-cache url)]
-    (if from-cache 
-      (do
-        (logger/info "found url in cache")
-        from-cache)
-      (do
-        (logger/info "fetching and caching url") 
-        (cache-and-get url)))))
-
 (defn get-titles [url]
-  (map html/text (html/select (find-resource url) [:h1])))
+  (map html/text (html/select (data/find-resource url) [:h1])))
 
 (defn find-title
   [url]
@@ -81,38 +25,67 @@
 
 (defn loop-match-records
   []
-  (loop [records (data/select-from-db)]
-    (let [r (first records)
-          rtitle (:title r)
-          gtitle (find-title (:url r))]
-      (if (not= rtitle gtitle)
-        (println (str "id# " (:id r) " is bad\n\treal: " rtitle "\n\tfound: " gtitle))
-        (println (str "id# " (:id r) " is good")))
-      (recur (rest records)))))
+  (loop [rs (data/filtered-from-db)
+         r (first rs)
+         ret []]
+    (if (seq rs)
+     (let [rtitle (:title r)
+            tcs (titles/title-candidates (:url r))]
+       (if (some #(= rtitle %) tcs)
+          (recur 
+            (next rs)
+            (second rs)
+            (conj ret (assoc r :success true)))
+          (recur
+            (next rs)
+            (second rs)
+            (conj ret (assoc r :success false :found tcs)))))
+     ret)))
 
 (defn match-records
   []
   (let [results []]
-    (for [r (data/select-from-db)]
+    (for [r (data/filtered-from-db)]
       (let [rtitle (:title r)
-            gtitle (find-title (:url r))]
-        (if (not= rtitle gtitle)
-          (assoc r :success false :found gtitle)
-          (assoc r :success true))))))
+            title-candidates (titles/title-candidates (:url r))]
+        (if (some #(= rtitle %) title-candidates)
+          (assoc r :success true)
+          (assoc r :success false :found title-candidates))))
+    results))
 
 (defn print-records
   []
-  (for [r (data/select-from-db)]
+  (for [r (data/filtered-from-db)]
     (str "id=" (:id r) " url=" (:url r))))
+
+(defn good [r] (str "good: " (:title r)))
+(defn bad [r]  (str "bad for: " (:url r) "\n\texpected: " (:title r) "\n\tfound: " (:found r)))
+
+(defn good-or-bad
+  [m r]
+  (if (:success r)
+    (assoc m :good (inc (:good m)))
+    (assoc m :bad (inc (:bad m)))))
 
 (defn report
   []
-  (loop [rs (match-records)
-         rp []]
-    (if (:success r)
-      (recur (next 
-      (conj rp (str "good: " (:title r)))
-      (conj rp (str "bad for: " (:url r) "\n\texpected: " (:title r) "\n\tfound: " (:found r)))))
-    (into [] rp))  
+  (reduce good-or-bad {:good 0 :bad 0} (loop-match-records)))
 
+(defn test-matches
+  []
+  (loop [rs (loop-match-records)
+         r (first rs)
+         good 0
+         bad 0]
+    (if (seq rs)
+      (if (:success r)
+        (recur (next rs) (second rs) (inc good) bad)
+        (recur (next rs) (second rs) good (inc bad)))
+      {:good good :bad bad})))
 
+(defn save-misses
+  "save a formatted map of urls that failed to match a candidate title"
+  []
+  (with-open [w (jio/writer "output.txt")]
+    (pprint/pprint (filter #(not (:success %)) (loop-match-records)) w)))
+    
--- src/cmarks/data.clj
+++ src/cmarks/data.clj
@@ -1,15 +1,22 @@
 (ns cmarks.data
-  (:use clojure.contrib.sql))
+  (:use clojure.contrib.sql)
+  (:require
+    [net.cgrand.enlive-html :as html]
+    [clojure.string :as string]
+    [clojure.java.io :as jio]
+    [clj-http.client :as client]))
+
+; database access
 
 (def db
   (let [db-host "localhost"
 	      db-port 3306
 	      db-name "redmarks"]
-  {:classname "com.mysql.jdbc.Driver"
-	           :subprotocol "mysql"
-	           :subname (str "//" db-host ":" db-port "/" db-name)
-	           :user "flyer"
-	           :password "daytona"}))
+    {:classname "com.mysql.jdbc.Driver"
+     :subprotocol "mysql"
+     :subname (str "//" db-host ":" db-port "/" db-name)
+     :user "flyer"
+     :password "daytona"}))
 
 ;https://github.com/swannodette/second-post/blob/master/src/second_post/mysql.clj
 (def ds
@@ -29,10 +36,88 @@
      (.setMinEvictableIdleTimeMillis 20000)
      (.setRemoveAbandoned true)
      (.setRemoveAbandonedTimeout 10)
-     (.setLogAbandoned true))})
+     (.setLogAbandoned false)
+     (.setLogWriter (java.io.PrintWriter. "/dev/null")))})
 
 (defn select-from-db
   []
   (with-connection ds
     (with-query-results rs ["select * from articles_read_article"]
       (into [] rs))))
+
+(defn find-article-by-id
+  [id]
+  (with-connection ds
+    (with-query-results rs ["select * from articles_read_article where id = ?" id]
+      (first rs))))
+
+; these just muddy up the data
+(def bad-ids
+  #{
+   54 ;gone from the web
+   24 ;bad https
+   27 ;weird 403 i can't repro outside of java.net.URL
+   1 ; new yorker with tricky title
+ })
+
+(defn filtered-from-db
+  []
+  (filter #(not (contains? bad-ids (:id %))) (select-from-db)))
+
+; URL & caching
+
+(def *cache-dir*
+  ; create dir and return dir name
+  (do
+    (doto (java.io.File. "cache/") (.mkdir))
+    "cache/"))
+
+(defn strip-protocol
+  [url]
+  (cond 
+    (.startsWith url "https://") (string/replace url "https://" "")
+    (.startsWith url "http://") (string/replace url "http://" "")))
+
+(defn strip-slash
+  [url]
+  ;(if (.endsWith url "/") (.substring url 0 (dec (.length url)))))
+  (string/replace url "/" "."))
+
+(defn url-to-cachename
+  [url] ;string
+  (str *cache-dir*
+    (strip-slash (strip-protocol url))))
+
+(defn find-in-cache
+  [url] ;str
+  (let [f (jio/as-file (url-to-cachename url))]
+    (if (.exists f)
+      (html/html-resource f)
+      nil)))
+
+(defn cache-url
+  [url] ;string
+  (try
+    (jio/copy 
+      (:body (client/get url)) 
+      (jio/as-file (url-to-cachename url)))
+    (catch  java.net.MalformedURLException e
+      (println (str "MalformedURL for [" url "]")))
+    (catch java.io.IOException e
+      (println (str "IO error for [" url "] " (.getMessage e))))))
+
+(defn cache-and-get
+  [url] ;str
+  (do
+    (cache-url url)
+    ;saves to disk, then reads back in
+    ;a little extra work, but no biggie
+    (find-in-cache url)))
+
+(defn find-resource
+  [url]
+  (let [from-cache (find-in-cache url)]
+    (if from-cache 
+      from-cache
+      (cache-and-get url))))
+
